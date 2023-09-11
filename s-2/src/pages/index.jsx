114ca281977508d949c463678647676447442970
benrobo <alumonabenaiah71@gmail.com>
2023-09-11 10:34:57 +0100

    updated posters card

--- s-2/src/pages/index.jsx
+++ s-2/src/pages/index.jsx
@@ -20,20 +20,22 @@ function Home() {
 
   return (
     <Layout>
-      <div className="w-full h-full min-h-[200px] md:min-h-[600px] relative bg-dark-200 flex flex-col items-center justify-center ">
+      <div className="w-full h-full min-h-[600px] md:min-h-[600px] relative bg-dark-200 flex flex-col items-center justify-center ">
         {/* navbar */}
-        <div className="w-full max-w-[80%] mx-auto min-h-[80px] absolute top-2 px-4 flex items-center justify-between gap-10">
+        <div className="w-full md:max-w-[80%] mx-auto min-h-[80px] absolute top-2 px-4 flex items-center justify-between gap-10 z-upper">
           <div className="w-auto flex items-center justify-center gap-4">
             <Image
               src={logoImg}
-              className={"w-[50px] h-[50px] "}
+              className={"w-[30px] h-[30px] md:w-[50px] md:h-[50px] "}
               width={50}
               height={50}
               alt="image"
             />
-            <p className="text-white-100 font-ppB">MovieBox</p>
+            <p className="text-white-100 font-ppB text-[14px] md:text-[20px] ">
+              MovieBox
+            </p>
           </div>
-          <div className="w-auto md:min-w-[525px] ">
+          <div className="w-auto md:min-w-[525px] hidden md:visible ">
             <InputGroup>
               <Input
                 placeholder="What do you want to watch?"
@@ -58,8 +60,8 @@ function Home() {
         {/* Poster */}
         <PosterComponent />
 
-        {/* pagination box */}
-        <div className="w-auto h-full flex flex-col items-center justify-center absolute right-0 ">
+        {/* pagination box (medium screen) */}
+        <div className="w-auto h-full flex flex-col items-center justify-center absolute right-0 invisible md:visible ">
           <div className="w-auto flex flex-col items-start justify-start gap-2 px-6">
             {pagination?.map((p) => (
               <button
@@ -81,6 +83,18 @@ function Home() {
             ))}
           </div>
         </div>
+
+        {/* Pagination box (small screen) */}
+        <div className="w-screen flex items-center justify-center gap-2 py-3 absolute bottom-1 visible  md:hidden ">
+          {pagination?.map((p) => (
+            <button
+              className={twMerge(
+                "w-[20px] rounded-[30px] mr-2 p-[2px] ",
+                p.active ? "bg-white-100" : "bg-white-300 opacity-[.4]"
+              )}
+            ></button>
+          ))}
+        </div>
       </div>
     </Layout>
   );
@@ -90,21 +104,23 @@ export default Home;
 
 function PosterComponent() {
   return (
-    <div className="w-full h-[300px] md:h-[600px] ">
+    <div className="w-full h-[650px] md:min-h-[600px] ">
       <Image
         src={posterImg}
-        className={"w-screen h-full object-cover "}
+        className={"w-screen h-screen md:h-full object-cover "}
         alt="image"
+        width={0}
+        height={0}
       />
 
-      <div className="w-screen h-full absolute top-[1em] flex items-center ">
-        <div className="w-full max-w-[80%] mx-auto flex items-center justify-between ">
+      <div className="w-screen h-full absolute top-[8em] md:top-[1em] flex items-center ">
+        <div className="w-full px-6 md:max-w-[80%] mx-auto flex items-center justify-between ">
           {/* description box */}
           <div
             id="left"
             className="w-full md:w-[404px] h-auto flex items-start justify-start flex-col gap-[16px] "
           >
-            <h1 className="text-white-100 text-[48px] leading-[56px] font-ppB ">
+            <h1 className="text-white-100 text-[20px] md:text-[48px] leading-[56px] font-ppB ">
               John Wick 3 : Parabellum
             </h1>
             <div className="w-full flex items-center justify-start gap-10">
@@ -121,24 +137,24 @@ function PosterComponent() {
                 </span>
               </div>
             </div>
-            <p className="text-white-100 text-[14px] font-dmsans ">
+            <p className="text-white-100 text-[12px] md:text-[14px] font-dmsans ">
               John Wick is on the run after killing a member of the
               international assassins' guild, and with a $14 million price tag
               on his head, he is the target of hit men and women everywhere.
             </p>
             <Button
               leftIcon={<PlayIcon />}
-              className="font-dmsans "
+              className="font-dmsans text-[12px] "
               style={{
                 background: "#BE123C",
                 color: "#fff",
-                fontSize: "14px",
+                // fontSize: "14px",
               }}
               _hover={{
                 opacity: 0.8,
               }}
             >
-              WATCH TRAILER
+              <span className="text-[12px] md:text-[14px] ">WATCH TRAILER</span>
             </Button>
           </div>
         </div>
