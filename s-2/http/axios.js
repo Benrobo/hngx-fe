b0ca9f721600b14e4671ff029d4029ff6e3a811a
benrobo <alumonabenaiah71@gmail.com>
2023-09-12 12:40:33 +0100

    updated

--- s-2/http/axios.js
+++ s-2/http/axios.js
@@ -1,21 +1,3 @@
-// const axios = require("axios");
-
-// const baseUrl =
-//   process.env.NODE_ENV !== "production" ? "" : "http://localhost:3000/api";
-
-// // axios config for client
-// const $axios = axios.create({
-//   baseURL: baseUrl,
-//   timeout: 30000,
-//   headers: {
-//     "Content-Type": "application/json",
-//     "Access-Control-Allow-Origin": "*",
-//   },
-//   withCredentials: true,
-// });
-
-// module.exports = $axios;
-
 const axios = require("axios");
 
 // axios config for server
