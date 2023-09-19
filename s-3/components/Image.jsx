ba4e30a8cf0a388b826cbfd7277cef0c20bffe39
Benrobo <alumonabenaiah71@gmail.com>
2023-09-20 00:21:51 +0100

    updated gallery

--- s-3/components/Image.jsx
+++ s-3/components/Image.jsx
@@ -1,7 +1,6 @@
 import NextImage from "next/image";
 import React, { useState } from "react";
 import { Blurhash } from "react-blurhash";
-import { motion } from "framer-motion";
 
 function ImageTag({ src, width, height, alt, className, style, ...props }) {
   return (
